72437b2fbe39884d2f5585ddbf21984d41eef0ca
rsinghvi-dev <rksb01@gmail.com>
2022-11-03 19:46:03 -0400

    changes to the tds sensor, still a bit buggy

--- sensor_package_testing/tds_sensor.py
+++ sensor_package_testing/tds_sensor.py
@@ -1,31 +1,34 @@
 from machine import ADC
-from time import sleep, monotonic
+import time
+
+# import monotonic
 
 v_ref = 5
 scount = 30
 
-adc = ADC(pin_no)
+adc = ADC(27)
 analog_buffer = []
 analog_buffer_temp = []
 copy_index = 0
 average_voltage = 0
 tds_value = 0
 temperature = 25
+count = 0
         
 while True:
-    analog_sample_timepoint = monotonic()
-    if (monotonic() - analog_sample_timepoint) > 0.04:
-        analog_sample_timepoint = monotonic()
-        count = 0
-        analog_buffer.append(adc.read_u16())
+    analog_sample_timepoint = time.time()
+    if (time.time() - analog_sample_timepoint) > 0.04:
+        analog_sample_timepoint = time.time()
+        analog_buffer.insert(count, adc.read_u16())
+        count+=1
         if count == scount:
-            analog_buffer = []
-            analog_buffer = 0
-    print_timepoint = monotonic()
-    if (monotonic() - print_timepoint) > 0.8:
-        print_timepoint = monotonic()
-        for i in range(scount):
-            analog_buffer_temp[i] = analog_buffer[i]
+#             analog_buffer = []
+            count = 0
+    print_timepoint = time.time()
+    if (time.time() - print_timepoint) > 0.8:
+        print_timepoint = time.time()
+        for item in analog_buffer:
+            analog_buffer_temp.append(item)
         average_voltage = analog_buffer_temp.sort()
         mid = len(average_voltage) // 2
         average_voltage = (average_voltage[mid] + average_voltage[~mid]) / 2
