302988d7099be4f2b48dffdd445484f43a8f53b8
rsinghvi-dev <rksb01@gmail.com>
2023-03-29 15:00:41 -0400

    bubbler test code

---
+++ testing/sensor_package_testing/bubbler_test.py
@@ -0,0 +1,14 @@
+from machine import Pin, ADC
+from time import sleep
+import math
+
+bubbler_relay = Pin(0, Pin.OUT)
+
+
+while True:
+    print("bubbler on?")
+    bubbler_relay.value(1)
+    sleep(20)
+    print("bubbler off?")
+    bubbler_relay.value(0)
+    sleep(5)
--- testing/sensor_package_testing/ph_sensor.py
+++ testing/sensor_package_testing/ph_sensor.py
@@ -1,16 +1,18 @@
-from machine import ADC
+from machine import Pin, ADC
 from time import sleep
 import math
 
-
-adc = ADC(28)
-
+# adc_pin = Pin(26, mode=Pin.IN)
+# adc = ADC(adc_pin)
+adc = ADC(2)
 
 while True:
-    voltage = adc.read_u16()/16.004
-    PH = float((-3*voltage)/653.0) + 15.6922
-    print("Voltage:%.4f " % (voltage))
-    print("PH:%.2f " % (PH))
-    sleep(1)
+    print(adc.read_u16())
+#     voltage = adc.read_u16()*5.0/65535
+#     PH = float((-3*voltage)/653.0) + 15.6922
+#     print(voltage)
+#     print("Voltage:%.4f " % (voltage))
+#     print("PH:%.2f " % (PH))
+    sleep(0.5)
 
 
